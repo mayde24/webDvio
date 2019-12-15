e3dfc90392e634ae31ad9a3007749a1d2a81a79f
Armand <begue.armand@gmail.com>
2019-12-15 16:10:45 +0100

    Edit Work & About

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -7,15 +7,15 @@ import {VariableService} from '../../services/variable.service';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
-  personIndex = 1 ;
-  hauteurEcran: number;
-  largeur: number;
+
+  hovered: number = 0;
+  personIndex: number = 1 ;
+  nbPersons: number = 4;
+
 
   constructor(public variableService: VariableService) { }
 
   ngOnInit() {
-    this.hauteurEcran = window.innerHeight;
-    this.largeur = window.innerWidth;
 
     if (document.querySelector('.contact-container2') === null) {
       document.querySelector('body').style.backgroundColor = 'white' ;
@@ -23,75 +23,20 @@ export class AboutComponent implements OnInit {
     }
   }
 
-  // sleep time expects milliseconds
-   sleep(time) {
-    return new Promise((resolve) => setTimeout(resolve, time));
+  hover(person: number) {
+    this.hovered = person;
   }
-  person1OverCall() {
-    document.getElementById('person2').classList.add('hidden');
-    document.getElementById('person3').classList.add('hidden');
-    document.getElementById('person1-text').classList.remove('hidden');
-    document.getElementById('person1-text').classList.remove('opacity');
+
+  leave() {
+    this.hovered = 0;
   }
-  person1LeaveCall() {
-    document.getElementById('person1-text').classList.add('opacity');
-    document.getElementById('person1-text').classList.add('hidden');
-    document.getElementById('person2').classList.remove('hidden');
-    document.getElementById('person3').classList.remove('hidden');
+
+  arrowRight() {
+    this.personIndex++;
   }
-  person2OverCall() {
-    document.getElementById('person1').classList.add('hidden');
-    document.getElementById('person3').classList.add('hidden');
-    document.getElementById('person2-left-text').classList.remove('hidden');
-    document.getElementById('person2-right-text').classList.remove('hidden');
-    document.getElementById('person2-left-text').classList.remove('opacity');
-    document.getElementById('person2-right-text').classList.remove('opacity');
-  }
-  person2LeaveCall() {
-    document.getElementById('person1').classList.remove('hidden');
-    document.getElementById('person3').classList.remove('hidden');
-    document.getElementById('person2-right-text').classList.add('opacity');
-    document.getElementById('person2-left-text').classList.add('opacity');
-    document.getElementById('person2-left-text').classList.add('hidden');
-    document.getElementById('person2-right-text').classList.add('hidden');
-  }
-  person3OverCall() {
-      document.getElementById('person1').classList.add('hidden');
-      document.getElementById('person2').classList.add('hidden');
-      document.getElementById('person3-text').classList.remove('hidden');
-      document.getElementById('person3-text').classList.remove('opacity');
-  }
-  person3LeaveCall() {
-      document.getElementById('person1').classList.remove('hidden');
-      document.getElementById('person2').classList.remove('hidden');
-      document.getElementById('person3-text').classList.add('opacity');
-      document.getElementById('person3-text').classList.add('hidden');
-  }
-  arrowRight3Call() {
-    if (document.getElementById(`element${this.personIndex + 1}`) != null) {
-      const pastReview = document.getElementById(`element${this.personIndex}`);
-      this.personIndex = this.personIndex + 1;
-      const myReview = document.getElementById(`element${this.personIndex}`);
-      pastReview.classList.add('hidden');
-      myReview.classList.remove('hidden');
-      document.getElementById('arrow-left3').classList.remove('opacity');
-    }
-    if (document.getElementById(`element${this.personIndex + 1}`) === null) {
-      document.getElementById('arrow-right3').classList.add('opacity');
-    }
-  }
-  arrowLeft3Call() {
-    if (document.getElementById(`element${this.personIndex - 1}`) != null) {
-      const pastReview = document.getElementById(`element${this.personIndex}`);
-      this.personIndex = this.personIndex - 1;
-      const myReview = document.getElementById(`element${this.personIndex}`);
-      pastReview.classList.add('hidden');
-      myReview.classList.remove('hidden');
-      document.getElementById('arrow-right3').classList.remove('opacity');
-    }
-    if (document.getElementById(`element${this.personIndex - 1}`) === null) {
-      document.getElementById('arrow-left3').classList.add('opacity');
-    }
+
+  arrowLeft() {
+    this.personIndex--;
   }
 }
 
