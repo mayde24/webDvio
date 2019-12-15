776bbba784a8b645ac34b46da3fbe9d0ee75f805
Armand <begue.armand@gmail.com>
2019-12-15 18:18:24 +0100

    Recup Work TS

--- src/app/components/work/work.component.ts
+++ src/app/components/work/work.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {VariableService} from '../../services/variable.service';
 
 @Component({
   selector: 'app-work',
@@ -7,112 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkComponent implements OnInit {
 
-  arrowLeft = document.getElementById('arrow-left');
-  arrowRight = document.getElementById('arrow-right');
-  i = 1 ;
+  reviewIndex = 1 ;
+  nbReviews: number = 3;
+  workIndex = 1 ;
+  nbWorks: number = 3;
 
-  arrowLeft2 = document.getElementById('arrow-left2');
-  arrowRight2 = document.getElementById('arrow-right2');
-  y = 1 ;
-
-  constructor() { }
+  constructor(public variableService: VariableService) { }
 
   ngOnInit() {
-    const num2 = window.innerWidth;
-    if (document.querySelector('.contact-container2') === null) {
-      document.querySelector('body').style.backgroundColor = 'white' ;
-      document.querySelector('body').style.maxHeight  = 'auto';
-    }
-    if (num2 > 500) {
-      document.getElementById('deskop-screen').classList.remove('hidden');
-      document.getElementById('phone-screen').classList.add('hidden');
-    } else {
-      document.getElementById('phone-screen').classList.remove('hidden');
-      document.getElementById('deskop-screen').classList.add('hidden');
-    }
-    if (document.querySelector('.work') != null) {
-      document.querySelector('body').style.maxHeight  = '100vh';
-      const myHeader = document.getElementsByClassName('header')[0] as HTMLElement;
-      const myDiv = document.getElementsByClassName('work')[0] as HTMLElement;
-      const myDiv2 = document.getElementsByClassName('work')[1] as HTMLElement;
-      const myDiv3 = document.getElementsByClassName('work')[2] as HTMLElement;
-      myDiv.style.marginTop  = myHeader.offsetHeight + 'px';
-      myDiv2.style.marginTop  = myHeader.offsetHeight + 'px';
-      myDiv3.style.marginTop  = myHeader.offsetHeight + 'px';
-    }
-    if (document.querySelector('.client') != null) {
-      document.querySelector('body').style.maxHeight  = '100vh';
-      const myHeader = document.getElementsByClassName('header')[0] as HTMLElement;
-      const myDiv = document.getElementsByClassName('client')[0] as HTMLElement;
-      const myDiv2 = document.getElementsByClassName('client')[1] as HTMLElement;
-      const myDiv3 = document.getElementsByClassName('client')[2] as HTMLElement;
-      myDiv.style.marginTop  = myHeader.offsetHeight + 'px';
-      myDiv2.style.marginTop  = myHeader.offsetHeight + 'px';
-      myDiv3.style.marginTop  = myHeader.offsetHeight + 'px';
-    }
-    window.addEventListener('scroll', (event) => {
-      if (window.scrollY > 400) {
-        document.getElementById('header').classList.add('header-shadow');
-      } else {
-        document.getElementById('header').classList.remove('header-shadow');
-      }
-    });
   }
 
-  arrowRightCall() {
-    if (document.getElementById(`review${this.i + 1}`) != null) {
-      const pastReview = document.getElementById(`review${this.i}`);
-      this.i = this.i + 1;
-      const myReview = document.getElementById(`review${this.i}`);
-      pastReview.classList.add('hidden');
-      myReview.classList.remove('hidden');
-      document.getElementById('arrow-left').classList.remove('opacity');
-    }
-    if (document.getElementById(`review${this.i + 1}`) === null) {
-      document.getElementById('arrow-right').classList.add('opacity');
-    }
+  arrowRightReview() {
+    this.reviewIndex++;
   }
 
-  arrowLeftCall() {
-    if (document.getElementById(`review${this.i - 1}`) != null) {
-      const pastReview = document.getElementById(`review${this.i}`);
-      this.i = this.i - 1;
-      const myReview = document.getElementById(`review${this.i}`);
-      pastReview.classList.add('hidden');
-      myReview.classList.remove('hidden');
-      document.getElementById('arrow-right').classList.remove('opacity');
-    }
-    if (document.getElementById(`review${this.i - 1}`) === null) {
-      document.getElementById('arrow-left').classList.add('opacity');
-    }
+  arrowLeftReview() {
+    this.reviewIndex--;
   }
 
-  arrowRight2Call() {
-      if (document.getElementById(`work${this.y + 1}`) != null) {
-        const pastReview = document.getElementById(`work${this.y}`);
-        this.y = this.y + 1;
-        const myReview = document.getElementById(`work${this.y}`);
-        pastReview.classList.add('hidden');
-        myReview.classList.remove('hidden');
-        document.getElementById('arrow-left2').classList.remove('opacity');
-      }
-      if (document.getElementById(`work${this.y + 1}`) === null) {
-        document.getElementById('arrow-right2').classList.add('opacity');
-      }
+  arrowRight2Work() {
+    this.workIndex++;
   }
 
-  arrowLeft2Call() {
-    if (document.getElementById(`work${this.y - 1}`) != null) {
-      const pastReview = document.getElementById(`work${this.y}`);
-      this.y = this.y - 1;
-      const myReview = document.getElementById(`work${this.y}`);
-      pastReview.classList.add('hidden');
-      myReview.classList.remove('hidden');
-      document.getElementById('arrow-right2').classList.remove('opacity');
-    }
-    if (document.getElementById(`work${this.y - 1}`) === null) {
-      document.getElementById('arrow-left2').classList.add('opacity');
-    }
+  arrowLeft2Work() {
+    this.workIndex--;
   }
 }
 
