a04f210a9ea101c3ef98e454d3fd83a20f9293dc
mayde24 <maya.douadi@outlook.fr>
2020-01-19 16:02:59 +0100

    Routes devis & estimation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,8 @@ import {ContactComponent} from './components/contact/contact.component';
 import {AboutComponent} from './components/about/about.component';
 import {Client1Component} from './components/client1/client1.component';
 import {Client2Component} from './components/client2/client2.component';
+import {DevisComponent} from './components/devis/devis.component';
+import {EstimationComponent} from './components/estimation/estimation.component';
 
 
 const routes: Routes = [
@@ -15,6 +17,8 @@ const routes: Routes = [
   { path: 'about', component: AboutComponent },
   { path: 'work/dame-de-coeur', component: Client1Component },
   { path: 'work/esmel-lian', component: Client2Component },
+  { path: 'devis', component: DevisComponent },
+  { path: 'estimation', component: EstimationComponent },
   { path: '**', redirectTo: 'home' }
 ];
 
