307efed228d6cfff3cb5526cabc1065d52609243
mayde24 <maya.douadi@outlook.fr>
2020-01-20 14:38:24 +0100

    Estimation design done

--- src/app/components/estimation/estimation.component.ts
+++ src/app/components/estimation/estimation.component.ts
@@ -10,24 +10,32 @@ export class EstimationComponent implements OnInit {
 
   form: FormGroup;
   questions = [
-    ["question1", "Site vitrine : 1", 1, 200, 500],
-    ["question2", "Assistance rédaction : 1", 1, 200, 500],
-    ["question3", "Création de grapismes : 1", 1, 100, 200],
-    ["question4", "Fonctionnalités + : 1", 1, 300, 1000],
-    ["question5", "Fonctionnalités ++ : 2", 2, 100, 500],
-    ["question6", "Pages > 5 : 2", 2, 200, 600],
-    ["question7", "Assistance mensuelle : 3", 3, 100, 300],
-    ["question8", "Logo : 3", 3, 200, 300]
+    ["question1", "Est-ce une création de site à partir d'aucun code pré existent ?", 1, 200, 500],
+    ["question2", "Aurez-vous besoin d'une assistance à la rédaction de votre site ?", 1, 200, 500],
+    ["question3", "Si vous n'avez pas déjà de maquette représentant votre projet, souhaitez-vous que nous prenions en charge le design du site ? (Réalisation de maquette avec confirmation de votre part en amont)", 1, 100, 200],
+    ["question4", "Est-ce que des fonctionnalités de base seront necessaires ? (Formulaire de contact, FAQ, carte Maps situant vos locaux)", 1, 300, 1000],
+    ["question5", "Est-ce que des fonctionnalités avancées seront necessaires ? (Newsletter, site multilingue, création et connexion de comptes, post utilisateurs)", 2, 100, 500],
+    ["question6", "Votre site sera-t-il consituté de plus de 5 pages ?", 2, 200, 600],
+    ["question7", "Voulez-vous souscrire à une assistance mensuelle ? (Rapports mensuels des vues de votre site, aide à la résolution de problèmes futurs)", 3, 100, 300],
+    ["question8", "Avez-vous besoin d'un logo ou d'un nouvel emblème pour votre projet ?", 3, 200, 300]
   ];
   choix: number = 1;
   min: number = 800;
   max: number = 1500;
   submitted: boolean = false;
+  index: number = 1;
+  largeur: number = 0;
+  parsec: number;
+  widthEcran: number;
+  thereIsNext: boolean = false;
+  thereIsPrevious: boolean = false;
 
   constructor(
     private formBuilder: FormBuilder,) { }
 
   ngOnInit() {
+    this.widthEcran = window.innerWidth;
+    this.parsec = (document.getElementById('entierParsec').offsetWidth / 6.0) - 5;
     this.form = this.formBuilder.group({
       question1: ['', Validators.required],
       question2: ['', Validators.required],
@@ -40,12 +48,64 @@ export class EstimationComponent implements OnInit {
     });
   }
 
-  onSubmit() {
-    for (let i = 1; i <= this.questions.length; i++) {
-      this.choix = Math.max(this.choix, this.form.controls["question" + i].value);
-      this.min += this.form.controls["question" + i].value > 0 ? Number(this.questions[i][3]) : 0;
-      this.max += this.form.controls["question" + i].value > 0 ? Number(this.questions[i][4]) : 0;
+
+  next() {
+    // @ts-ignore
+    if (this.index != 7 && ((document.getElementById(`question${this.index + 1}_oui`) as HTMLElement).checked || (document.getElementById(`question${this.index + 1}_non`) as HTMLElement).checked)) {
+      this.index++;
+      for (let pas = 0; pas < this.parsec; pas++) {
+        setTimeout(() => {
+          this.largeur++;
+          (document.getElementById('progress') as HTMLElement).style.width = this.largeur + 'px';
+        }, 100);
+      }
+      this.thereIsPrevious = true;
+      // @ts-ignore
+      if (((document.getElementById(`question${this.index + 1}_oui`) as HTMLElement).checked == false && (document.getElementById(`question${this.index + 1}_non`) as HTMLElement).checked == false)) {
+        this.thereIsNext = false;
+      }
     }
-    this.submitted = true;
+  }
+  previous() {
+    if (this.index != 1) {
+      this.index--;
+      for (let pas = 0; pas < this.parsec; pas++) {
+        setTimeout(() => {
+          this.largeur--;
+          (document.getElementById('progress') as HTMLElement).style.width = this.largeur + 'px';
+        }, 100);
+      }
+      this.thereIsNext = true;
+    }
+    if(this.index == 1) {
+      this.thereIsPrevious = false;
+    }
+  }
+  nextForced() {
+    setTimeout( () =>{
+      if (this.index != 7) {
+        this.index++;
+        for (let pas = 0; pas < this.parsec; pas++) {
+          setTimeout(() => {
+            this.largeur++;
+            (document.getElementById('progress') as HTMLElement).style.width = this.largeur + 'px';
+          }, 100);
+        }
+        this.thereIsPrevious = true;
+        // @ts-ignore
+        if (((document.getElementById(`question${this.index + 1}_oui`) as HTMLElement).checked || (document.getElementById(`question${this.index + 1}_non`) as HTMLElement).checked)) {
+          this.thereIsNext = true;
+        } else {
+          this.thereIsNext = false;
+        }
+      } else {
+        for (let i = 1; i <= this.questions.length; i++) {
+          this.choix = Math.max(this.choix, this.form.controls["question" + i].value);
+          this.min += this.form.controls["question" + i].value > 0 ? Number(this.questions[i][3]) : 0;
+          this.max += this.form.controls["question" + i].value > 0 ? Number(this.questions[i][4]) : 0;
+        }
+        this.submitted = true;
+      }
+    }, 500);
   }
 }
